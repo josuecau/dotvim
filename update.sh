ed2a1fdffa4bb56713e573348fbd59df7a8272d3
Josué Cau <josuecau@yahoo.fr>
2013-06-26 22:27:13 +0200

    Change update shell script output

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "\033[0;34mUpdate dotvim...\033[0m"
+echo "Update dotvim..."
 cd ~/.vim
 git checkout master
 git pull origin master
 git submodule foreach git pull origin master
 git submodule update
-echo "\033[0;34mDone.\033[0m"
+echo "Done."
