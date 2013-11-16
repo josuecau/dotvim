d0bda6e95795df390f691b56054fae0b8260a19d
Josué Cau <josuecau@yahoo.fr>
2013-11-16 22:09:30 +0100

    Simplify updater shell script

--- update.sh
+++ update.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-echo "Update dotvim..."
 cd ~/.vim
 git checkout master
-if git pull --rebase origin master
-then
-    git pull origin master
-    git submodule foreach git pull origin master
-    git submodule update --init
-    echo "Done."
-else
-    echo "An error occurred. Please try again later."
-fi
+git pull --rebase origin master
+git submodule update --init --recursive
