f2ce5a93e2053932403f15fc7dcac83206b70102
Josué Cau <josuecau@yahoo.fr>
2013-11-30 15:06:59 +0100

    Fix gitmodules update

--- update.sh
+++ update.sh
@@ -3,4 +3,6 @@
 cd ~/.vim
 git checkout master
 git pull --rebase origin master
+git submodule foreach git checkout origin master
+git submodule foreach git pull origin master
 git submodule update --init --recursive
